9c38b28ce9609597a5b75a450b1ef0ceea1f6f32
Armas Ramirez Carlos <MB80438@cb.bbvabancomer.com.mx>
2019-06-19 12:27:56 -0500

    Se agregaron correciones de checkstyle 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
 /**
- * This method returns a simple greeting using a name parameter
- * @param someone name of a person
- * @return a simple greeting String
+ * This method returns a simple greeting using a name parameter.
+ * @param someone name of a person.
+ * @return a simple greeting String.
  */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
